ce83791762a98303b9244841a38c478758c54237
Gabriel Samuel <samuelgab246@gmail.com>
2024-01-30 18:11:41 -0800

    selector

---
+++ style.js
@@ -0,0 +1,27 @@
+// var sam = document.getElementsByTagName('h1'); {
+// 	console.log(sam);
+// }
+
+// let samu = document.getElemnetsByClassName('second'); {
+// 	console.log(samu);
+// }
+
+// var gab = document.getElementById("first") [
+
+// 	console.log(gab)
+	
+// ]
+// document.getElementbyid("first")
+
+
+// 	alert(gab);
+
+// document.querySelector("li").getAttribute("random");
+
+// var h1 = document.querySelector("h1");
+
+// h1.className = "coolTitle";
+
+// document.querySelector("h1").classList.remove("coolTitle");
+
+// document.querySelector("h1").classList.remove("coolTitle");
